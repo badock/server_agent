8bf2c74f2db1f865a27e38799c76711841b64f76
Jonathan Pastor <jonathancmoa@gmail.com>
2018-09-04 01:25:56 +0200

    started ottd will be logged into a file

--- agent/ottd/server_agent.py
+++ agent/ottd/server_agent.py
@@ -83,7 +83,7 @@ class OttdAgentActions(AgentActions):
         task_uuid = str(uuid.uuid4())
         tmp_log_file = "/tmp/%s.log" % (task_uuid)
 
-        cmd = '/usr/games/openttd -D -d' % tmp_log_file
+        cmd = '/usr/games/openttd -D -d > %s 2>&1' % tmp_log_file
         proc = subprocess.Popen([cmd])
 
         self.tasks[task_uuid] = {
