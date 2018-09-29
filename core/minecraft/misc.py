bb3d9ca7bab7ceada8d4fae774944a0b5f28b231
badock <jonathan.pastor@me.com>
2018-09-29 21:59:37 +0000

    correct a bug with getting RCON IP and password for minecraft

--- core/minecraft/misc.py
+++ core/minecraft/misc.py
@@ -1,14 +1,15 @@
 import mcrcon
 
-SERVER_IP = "127.0.0.1"
+SERVER_IP = "172.17.0.2"
 ADMIN_PORT = 25575
 RCON_PASSWORD = "adminVYcm1Zfz"
 MINECRAFT_CONFIG_LOCATION = "/home/csgoserver/serverfiles/server.properties"
 
 
 def rcon(command, server_details):
+    server_ip, port = server_details.get("server", {}).get("server_ip", "127.0.0.1:25575").split(":")
     password = server_details.get("server", {}).get("rcon_password", None)
-    rcon = mcrcon.MCRcon(SERVER_IP, password, ADMIN_PORT)
+    rcon = mcrcon.MCRcon(server_ip, password, ADMIN_PORT)
     rcon.connect()
 
     result = rcon.command(command)
@@ -18,6 +19,6 @@ def rcon(command, server_details):
 
 
 if __name__ == "__main__":
-    prefix, players_str = rcon("list").split(" players online: ")
+    prefix, players_str = rcon("list", {"server": {"server_ip": "172.17.0.2:25565", "rcon_password": "XXXX"}}).split(" players online: ")
     players = players_str.split(", ")
     print(players)
